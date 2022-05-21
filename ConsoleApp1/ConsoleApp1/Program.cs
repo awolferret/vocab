7ce33d36f961e7958436f0fc2dbed21034ebcd9a
awolferret <104518827+awolferret@users.noreply.github.com>
2022-05-21 17:57:36 +0300

    1

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> food = new Dictionary<string, int>();
+            food.Add("Cheese", 1);
+            food.Add("Meet", 2);
+            food.Add("Water", 3);
+            Console.WriteLine("Введите ключ");
+            string key = Console.ReadLine();
+
+            if (food.ContainsKey(key))
+            {
+                Console.WriteLine(food[key]);
+            }
+            else 
+            {
+                Console.WriteLine("Свопадений нет");
+            }
+        }
+    }
+}
